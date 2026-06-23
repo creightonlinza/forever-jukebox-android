23070b60720786194c06c9024a441e17f9c52977
Creighton Linza <creightonlinza@gmail.com>
2026-06-23 10:22:57 -0400

    latest updates

--- app/src/main/java/com/foreverjukebox/app/ui/WhatsNew.kt
+++ app/src/main/java/com/foreverjukebox/app/ui/WhatsNew.kt
@@ -1,10 +1,10 @@
 package com.foreverjukebox.app.ui
 
 private val currentWhatsNewBullets = listOf(
-    "I got a fever, and the only prescription is More Cowbell! (new audio mode)",
-    "Loading sound effects added, enable in Settings menu",
+    "Added minimum branch length Tuning slider",
+    "Can now display YouTube thumbnail previews (long press on a YT search result)",
+    "New Audio Modes: 8-bit, Underwater, Cathedral & More Cowbell",
     "Favorites search and sorting added, with maximum saved favorites bumped to 150 tracks",
-    "A few more Audio Mode options introduced: 8-bit, Underwater, Cathedral",
     "Misc fixes and under the hood improvements",
     "Please report any bugs to GitHub/Reddit/Discord"
 )
