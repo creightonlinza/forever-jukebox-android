f57192d19f1e5376fcb0ff2c6f5f6e69171f12eb
Creighton Linza <creightonlinza@gmail.com>
2026-06-29 10:38:16 -0400

    update

--- app/src/play/java/com/foreverjukebox/app/ui/WhatsNewContent.kt
+++ app/src/play/java/com/foreverjukebox/app/ui/WhatsNewContent.kt
@@ -2,6 +2,8 @@ package com.foreverjukebox.app.ui
 
 internal val currentWhatsNewBullets = listOf(
     "Initial launch on the Play Store!",
+    "Tuning settings now auto-save per track",
+    "Added search and sort to the completed analysis list",
     "New Audio Modes: 8-bit, Underwater, Cathedral & More Cowbell",
     "Please report any bugs to GitHub/Reddit/Discord"
 )
