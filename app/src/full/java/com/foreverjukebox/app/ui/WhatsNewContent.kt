cb1d05a82853d35993a56e9a653f22a0d359b2b4
Creighton Linza <creightonlinza@gmail.com>
2026-07-19 22:36:56 -0400

    update

--- app/src/full/java/com/foreverjukebox/app/ui/WhatsNewContent.kt
+++ app/src/full/java/com/foreverjukebox/app/ui/WhatsNewContent.kt
@@ -1,12 +1,11 @@
 package com.foreverjukebox.app.ui
 
-import android.R.attr.track
-
 internal val currentWhatsNewBullets = listOf(
     "Added an Intensity customization slider for some Audio Modes",
     "Autocanonizer now loops by default when \"finish out the track\" is not selected",
     "Redesigned interface, fresh new look!",
     "Added minimum branch length Tuning slider",
     "Can now display YouTube thumbnail previews (long press on a YT search result)",
-    "Misc fixes and under the hood improvements"
+    "Misc fixes and under the hood improvements",
+    "Please report any bugs to GitHub/Reddit/Discord"
 )
